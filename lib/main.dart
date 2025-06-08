6c3aff5eef56c3a97b359771ebd65d609206e92e
Aimee-Gloire <i.aimeegloi@alustudent.com>
2025-06-08 22:23:03 +0200

    added a homescreen with an appbar and a basic body

--- lib/main.dart
+++ lib/main.dart
@@ -16,3 +16,21 @@ class StudentPortalApp extends StatelessWidget {
     );
   }
 }
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Student Portal'),
+        ),
+        body: const Center(
+          child: Text(
+            'Welcome to the Student Portal',
+          ),
+        ),
+        );
+  }
+}
